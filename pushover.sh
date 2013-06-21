82558e62b1327887b721f257d0799df70d151f13
Philipp Schmitt <philipp.schmitt@laxlinux.net>
2013-06-21 10:08:51 +0200

    Fixed: XDG_CONFIG_DIR may not be set on every system Create skeleton config file if not present Do not return 1 when invoking help (-h)

--- pushover.sh
+++ pushover.sh
@@ -4,31 +4,33 @@
 CURL="$(which curl)"
 PUSHOVER_URL="https://api.pushover.net/1/messages"
 TOKEN="" # Must be set in pushover.conf or given on command line
-USER="" # Must be set in pushover.conf
+USER=""  # Must be set in pushover.conf
 
 # Load user config
+[[ -n $XDG_CONFIG_HOME ]] || XDG_CONFIG_HOME=$HOME/.config
 CONFIG_FILE="${XDG_CONFIG_HOME-${HOME}/.config}/pushover.conf"
-if [ -e "${CONFIG_FILE}" ]; then
+if [[ -e "${CONFIG_FILE}" ]]; then
     . ${CONFIG_FILE}
 else
-    echo "Can't find ${CONFIG_FILE}: You must create it before using this script" >&2
+    echo -e 'TOKEN="" #your application token\nUSER="" #your user key' > $CONFIG_FILE
+    echo "Please edit ${CONFIG_FILE}: User Key or Token not set." >&2
     exit 1
 fi
 
 # Functions used elsewhere in this script
 usage() {
-    echo "${0} <options> <message>"
+    echo "$1 <options> <message>"
     echo " -d <device>"
     echo " -p <priority>"
     echo " -t <title>"
-    echo " -T <token>"
-    exit 1
+    echo " -T <token>" 
 }
+
 opt_field() {
     field=$1
     shift
     value="${*}"
-    if [ ! -z "${value}" ]; then
+    if [[ -n "${value}" ]]; then
         echo "-F \"${field}=${value}\""
     fi
 }
@@ -39,34 +41,34 @@ priority=""
 title=""
 
 # Option parsing
-optstring="d:p:t:T:h"
-while getopts ${optstring} c; do
+while getopts "d:p:t:T:h" c; do
     case ${c} in
         d) device="${OPTARG}" ;;
         p) priority="${OPTARG}" ;;
         t) title="${OPTARG}" ;;
         T) TOKEN="${OPTARG}" ;;
-        [h\?]) usage ;;
+        [h\?]) usage $(basename $0); exit 0; ;;
     esac
 done
 shift $((OPTIND-1))
 
 # Is there anything left?
-if [ "$#" -lt 1 ]; then
-    usage
+if [[ "$#" -lt 1 ]]; then
+    usage $(basename $0)
+    exit 1
 fi
 message="$*"
 
 # Check for required config variables
-if [ ! -x "${CURL}" ]; then
+if [[ ! -x "${CURL}" ]]; then
     echo "CURL is unset, empty, or does not point to curl executable. This script requires curl!" >&2
     exit 1
 fi
-if [ -z "${TOKEN}" ]; then
+if [[ -z "${TOKEN}" ]]; then
     echo "TOKEN is unset or empty: Did you create ${CONFIG_FILE}?" >&2
     exit 1
 fi
-if [ -z "${USER}" ]; then
+if [[ -z "${USER}" ]]; then
     echo "USER is unset or empty: Did you create ${CONFIG_FILE}?" >&2
     exit 1
 fi
@@ -78,6 +80,6 @@ curl_cmd="\"${CURL}\" -s \
     $(opt_field title "${title}") \
     $(opt_field device "${device}") \
     $(opt_field priority "${priority}") \
-    ${PUSHOVER_URL} 2>&1 >/dev/null || echo \"$0: Failed to send message\" >&2"
-eval "${curl_cmd}" 
+    ${PUSHOVER_URL} 2>&1 >/dev/null" 
+eval "${curl_cmd}" || echo "$(basename $0): Failed to send message" >&2 
 

4e4a2a4e1295f7b8c42f87793d9f38958f1ac44a
Randall Hand <randall.hand@gmail.com>
2021-09-25 10:24:58 -0400

    Revert the Config Path change.

--- pushover.sh
+++ pushover.sh
@@ -121,7 +121,7 @@ done
 if [ ! -z "${PUSHOVER_CONFIG}" ]; then
     CONFIG_FILE="${PUSHOVER_CONFIG}"
 else
-    CONFIG_FILE="/etc/pushover.conf"
+    CONFIG_FILE="${XDG_CONFIG_HOME-${HOME}/.config}/pushover.conf"
 fi
 if [ -e "${CONFIG_FILE}" ]; then
     . "${CONFIG_FILE}"
